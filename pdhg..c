6eb5b2409f5d1e6da3785791c3d1597bdd6dee1e
6261vishnumho <7225vishnumahor@gmail.com>
2025-07-13 23:22:13 +0530

    add files

---
+++ pdhg..c
@@ -0,0 +1,32 @@
+# include <stdio.h>
+
+int main(){
+int i,j;
+for(i=1;i<=6; i++){
+    for(j=i;j<=6;j++){
+        printf(" ");
+    }
+    for(int k=1;k<=i;k++)
+    {
+        printf("*");
+    }
+ for(int k=1;k<i;k++)
+    {
+        printf("*");
+
+    if(k==i++){
+        printf("*");
+
+    }
+
+    else
+        {
+
+            printf(" ");
+        }
+    }
+printf("\n");
+}
+
+return 0;
+}
